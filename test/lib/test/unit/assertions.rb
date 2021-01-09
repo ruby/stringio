074f82d2d0cbfe3c85bdd6e4e57e69419cd2f928
Nobuyoshi Nakada <nobu@ruby-lang.org>
2021-01-09 14:36:31 +0900

    Return the result of the block given to assert_warning

--- test/lib/test/unit/assertions.rb
+++ test/lib/test/unit/assertions.rb
@@ -676,13 +676,15 @@ eom
       end
 
       def assert_warning(pat, msg = nil)
-        stderr = EnvUtil.verbose_warning {
-          EnvUtil.with_default_internal(pat.encoding) {
-            yield
+        result = nil
+        stderr = EnvUtil.with_default_internal(pat.encoding) {
+          EnvUtil.verbose_warning {
+            result = yield
           }
         }
         msg = message(msg) {diff pat, stderr}
         assert(pat === stderr, msg)
+        result
       end
 
       def assert_warn(*args)
