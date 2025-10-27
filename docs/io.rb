a94e4f311c20f79e9692679612dfa0c8bdc12937
Nobuyoshi Nakada <nobu@ruby-lang.org>
2025-10-27 16:27:32 +0900

    [DOC] Link `IO` to the official document

--- docs/io.rb
+++ docs/io.rb
@@ -1,8 +1,8 @@
-# :stopdoc:
+# The {built-in class for I/O}[https://docs.ruby-lang.org/en/master/IO.html].
 class IO
+  # :stopdoc:
   module generic_readable
   end
   module generic_writable
   end
 end
-# :startdoc:
