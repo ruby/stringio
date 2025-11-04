47de8f5401f3b2bd886ed1943508850b40d5cfca
BurdetteLamar <burdettelamar@yahoo.com>
2025-11-04 18:31:13 +0000

    [DOC] Doc for StringIO#size

--- ext/stringio/stringio.c
+++ ext/stringio/stringio.c
@@ -1998,10 +1998,10 @@ strio_syswrite_nonblock(int argc, VALUE *argv, VALUE self)
 
 /*
  * call-seq:
- *   strio.length -> integer
- *   strio.size   -> integer
+ *   size -> integer
  *
- * Returns the size of the buffer string.
+ * :include: stringio/size.rdoc
+ *
  */
 static VALUE
 strio_size(VALUE self)
